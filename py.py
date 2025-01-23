8f056fea4d22a31d389b0946dcf52f6cc976ef71
Mr Jumba <flaviajumba@gmail.com>
2025-01-24 00:42:55 +0300

    just learned about range

--- py.py
+++ py.py
@@ -1,15 +1,19 @@
-children = [12, 14, 15, 12, 13]
+# children = [12, 14, 15, 12, 13]
 
-total_sum = 0
+# total_sum = 0
 
-length = 0
+# length = 0
 
-for child in children:
+# for child in children:
 
-    total_sum += child  # Corrected this line to add the value of 'child'
+#     total_sum += child  # Corrected this line to add the value of 'child'
     
-    length += 1
+#     length += 1
 
-# Calculate average and print results
-average = total_sum / length if length > 0 else 0
-print("The total sum of the children is: " + str(total_sum) + " and the total average is: " + str(average))
+# # Calculate average and print results
+# average = total_sum / length if length > 0 else 0
+# print("The total sum of the children is: " + str(total_sum) + " and the total average is: " + str(average))
+children = [12,2,32,13,134,23]
+for child in range(len(children)):
+    add = child+10
+    print(add) 
