f605ece6a5e9d09035c9ea6a2d44ec7fc42f182b
Mr Jumba <flaviajumba@gmail.com>
2025-01-29 01:20:15 +0300

    captures the country codes of kenya

--- py.py
+++ py.py
@@ -1,10 +1,16 @@
-phonenumbers = ['2442526362','2025551212','2545551212','2315551212','2455551212','2315551212']
+phonenumbers = ['2442526362', '2025551212', '2545551212', '2545551212', '2455551212', '2315551212']
 
-def format_phone (phonenumbers):
-    codes = []
-    for e in phonenumbers:
-        ans = '+'+e[:3]
-        codes.append(ans)
+def format_phone(phonenumbers):
+    codes = []  # Creates an empty list to store codes
+    kenya_found = False  # Flag to track if Kenya code is found
+    for e in phonenumbers:  # Loops through each phone number
+        ans = '+' + e[:3]   # Takes first 3 digits and adds '+' prefix
+        if ans == '+254':   # Checks if the code IS '+254'
+            codes.append(ans)  # Adds the code if it IS '+254'
+            kenya_found = True  # Set the flag to True if Kenya code is found
+    if not kenya_found:  # If no Kenya code was found
+        print('Kenya is not found here')  # Print message if no '+254'
     return codes
+
 area_codes = format_phone(phonenumbers)
-print("The country codes are:",area_codes)
+print("The country code is:", area_codes)
