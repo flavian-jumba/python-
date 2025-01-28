caf448ac3ac679a2ea60342f6cd60544ac28dd8d
Mr Jumba <flaviajumba@gmail.com>
2025-01-29 01:02:40 +0300

    just made some good updates to capture the individual codes of the country(just addded the +)

--- py.py
+++ py.py
@@ -3,7 +3,7 @@ phonenumbers = ['2442526362','2025551212','2545551212','2315551212','2455551212'
 def format_phone (phonenumbers):
     codes = []
     for e in phonenumbers:
-        ans = "(" + e[:3] + ")"
+        ans = '+'+e[:3]
         codes.append(ans)
     return codes
 area_codes = format_phone(phonenumbers)
