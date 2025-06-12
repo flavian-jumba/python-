e68dbef13bd04d76a9dce45963912701d9641683
flavian-jumba <flaviajumba@gmail.com>
2025-06-12 22:27:08 +0300

    did more practice on python

--- new.py
+++ new.py
@@ -1,3 +1,20 @@
-def students ():
+import requests
 
-    print("jumba")
+baseUrl = "https://pokeapi.co/api/v2/pokemon/"
+
+def get_pokemon(name):
+    url = f"{baseUrl}/{name}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("Error: Pokémon not found.")
+
+name = input("Enter the name of the Pokémon: ")
+pokemon= get_pokemon(name)
+
+if pokemon:
+    print(f"name: {pokemon['name']}")
+    print(f"height: {pokemon['height']}")
+    print(f"weight: {pokemon['weight']}")
+    print("Abilities:")
--- py.py
+++ py.py
@@ -1,2 +1,22 @@
-import new
-new.students()
+import requests 
+
+API = "05d508fe5fcc6829f2913e4537d4d6b8"
+baseUrl = "https://api.openweathermap.org/data/2.5/weather"
+
+def getWeatherInfo(cityName):
+    url = f"{baseUrl}?q={cityName}&appid={API}"
+    response= requests.get(url)
+    data = response.json()
+    if response.status_code == 200:
+        return data
+    else:
+        print("Error: Unable to fetch weather data.")
+
+cityName = input("Enter the city name: ")
+weather_info = getWeatherInfo(cityName)
+
+if weather_info:
+    print("Weather Information:")
+    print(f"City: {weather_info['name']}")
+    print(f"Temperature: {weather_info['main']['temp']} K")
+    print(f"Weather: {weather_info['weather'][0]['description']}")
